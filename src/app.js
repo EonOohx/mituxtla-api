0afcec170cb5c93e619026c6f6cc6f21964fb13b
EonOohx <RaulAlejandro_RodriguezR@hotmail.com>
2025-05-16 13:34:48 -0600

    Correct health endpint

--- src/app.js
+++ src/app.js
@@ -6,15 +6,14 @@ const app = express();
 
 app.use("/place", placesRoutes);
 
+app.use("/health", (req, res) => {
+  res.status(200).send('OK');
+});
+
 app.use((req, res, next) => {
   res.status(404).json({
     message: `endpoint ${req.path.replace("/", "")} not found`,
   });
 });
-
-app.get('/health', (req, res) => {
-  res.status(200).send('OK');
-});
-
 
 export default app;
